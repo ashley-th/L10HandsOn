48be08e174ca2b16ed3ca7cbbb44695509d48c3e
Ashley Thomas <ashley.th@hotmail.com>
2018-07-20 12:37:28 -0500

    The GitHub Repo Works!!!
    Also I cleaned up the html a little

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ $(document).ready(function () {
     });
 });
 
+
+let newRequest = new XMLHttpRequest();
+newRequest.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+        let myObj = JSON.parse(this.responseText);
+        for(var i=0; i < myObj.length; i++) {
+            addRepo(myObj[i].name);
+        }
+    }
+};
+
+newRequest.open("GET", "https://api.github.com/users/ashley-th/repos", true);
+newRequest.send();
+
+function addRepo(name) {
+    var node = document.createElement("li");
+    var textNode = document.createTextNode(name);
+    node.appendChild(textNode);
+    document.getElementById("repoList").appendChild(node);
+}
