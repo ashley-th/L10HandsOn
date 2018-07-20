fce61a3ad411101c982098d16f2ed71b0744dc81
Ashley Thomas <ashley.th@hotmail.com>
2018-07-20 11:04:41 -0500

    The hover buttons WORK!!!
    Keeping the icons in my project made it easier to target and manipulate them without weird errors coming up.

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".socMed").hover(function(){
+        $(this).css({"height": "60px", "width": "60px"});
+    },
+    function(){
+        $(this).css({"height": "45px", "width": "45px"});
+    });
+});
+
